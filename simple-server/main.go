435557c700f5afad522d200e911b014c42a0076b
nahK994 <nkskl6@gmail.com>
2024-07-19 19:09:05 +0600

    add Divide function

--- simple-server/main.go
+++ simple-server/main.go
@@ -1,6 +1,7 @@
 package main
 import (
 	"fmt"
+	"errors"
 	"net/http"
 )
 
@@ -20,11 +21,28 @@ func About(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main()  {
-	fmt.Println("Hello World!!!")
+func Divide(w http.ResponseWriter, r *http.Request) {
+	var value, err = divideValues(2,0)
+	if err != nil {
+		fmt.Fprintf(w, "Divided by 0 error")
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f / %f = %f", 2, 0, value))
+}
 
+func divideValues(x, y float32) (float32, error) {
+	if y == 0 {
+		err := errors.New("Cannot divided by 0")
+		return 0, err
+	}
+
+	return (x/y), nil
+}
+
+func main()  {
+	// fmt.Println("Hello World!!!")
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println("Starting application on port:", portNumber)
 	var uri = fmt.Sprintf(":%d", portNumber)
