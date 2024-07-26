a4b90d8f111781b071d7e1309ced8eaa6b7a0fdf
nahK994 <nkskl6@gmail.com>
2024-07-26 12:53:58 +0600

    init a few APIs

--- go-bank/api.go
+++ go-bank/api.go
@@ -1,9 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type APIServer struct {
 	listenAddr string
+}
+
+type APIError struct {
+	Error string
+}
+
+type apiFunc func(w http.ResponseWriter, r *http.Request) error
+
+func handleHTTPError(w http.ResponseWriter, status int, v any) error {
+	w.WriteHeader(status)
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(v)
+}
+
+func makeHTTPhandler(f apiFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := f(w, r); err != nil {
+			handleHTTPError(w, http.StatusBadRequest, APIError{Error: err.Error()})
+		}
+	}
 }
 
 func NewAPIServer(listenAddr string) *APIServer {
@@ -12,8 +38,22 @@ func NewAPIServer(listenAddr string) *APIServer {
 	}
 }
 
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/a", makeHTTPhandler(s.handleAccount))
+	http.ListenAndServe(s.listenAddr, router)
+}
+
 func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	if r.Method == "GET" {
+		return s.handleGetAccount(w, r)
+	} else if r.Method == "POST" {
+		return s.handleCreateAccount(w, r)
+	} else if r.Method == "DELETE" {
+		return s.handleDeleteAccount(w, r)
+	}
+	return fmt.Errorf("%s method not allowed", r.Method)
 }
 
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
--- go-bank/main.go
+++ go-bank/main.go
@@ -4,4 +4,6 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!!!")
+	server := NewAPIServer("127.0.0.1:8000")
+	server.Run()
 }
