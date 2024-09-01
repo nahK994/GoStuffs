3b047e1f9820a52f14a397b34679702746e2f96d
nahK994 <nkskl6@gmail.com>
2024-09-01 18:58:06 +0600

    create run.sh

--- book-store/cmd/web/main.go
+++ book-store/cmd/web/main.go
@@ -9,6 +9,6 @@ import (
 func main() {
 	r := routes.RegisterRoutes()
 
-	log.Println("Starting server on :8080...")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Println("Starting server on :8000")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
--- book-store/pkg/config/app.go
+++ book-store/pkg/config/app.go
@@ -4,9 +4,7 @@ import (
 	"book-store/pkg/models"
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -14,17 +12,14 @@ import (
 var DB *gorm.DB
 
 func InitDB() {
-	var err error
+	POSTGRES_USER := "user"
+	POSTGRES_PASSWORD := "password"
+	POSTGRES_DB := "book_store"
+	POSTGRES_PORT := 5432
+	POSTGRES_HOST := "localhost"
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Dhaka", POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT)
 
-	err = godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Dhaka",
-		os.Getenv("DB_HOST"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_DB"), os.Getenv("POSTGRES_PORT"))
-
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
