cbedab18bd6daa6a8fb0ab1f2bafb1b74a4ce7fd
nahK994 <nkskl6@gmail.com>
2024-09-01 19:42:50 +0600

    update run.sh in book-store

--- book-store/run.sh
+++ book-store/run.sh
@@ -3,7 +3,7 @@ echo "2) kill process"
 
 read -p "Type: " cmd
 if [[ $cmd == 1 ]]; then
-   go run cmd/main.go
+   go run cmd/web/main.go
 elif [[ $cmd == 2 ]]; then
     sudo kill -9 $(sudo lsof -t -i:8000)
 fi
